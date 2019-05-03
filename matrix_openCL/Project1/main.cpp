42d6171cf84fcbbdc428f52522b4caacc506dee6
Ristea Stefan <stefanristea1@yahoo.com>
2019-05-03 08:35:06 +0300

    Implemented lab5.2

--- matrix_openCL/Project1/main.cpp
+++ matrix_openCL/Project1/main.cpp
@@ -6,83 +6,92 @@
 #include <string>
 #include <vector>
 #include <array>
-using namespace std;
+
 using namespace cl;
 
-int main() {
+template <std::size_t M, std::size_t N>
+using Matrix = std::array<std::array<int, N>, M>;
+
+std::vector<Platform> platforms;
+cl_context_properties cps[3];
+std::vector<Device> devices;
+CommandQueue queue;
+
+std::ifstream sourceFile("vector_adds_kernel.cl");
+std::string sourceCode(
+    std::istreambuf_iterator<char>(sourceFile),
+    (std::istreambuf_iterator<char>()));
+Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
+Program program;
+
+void testLab5_2(const Matrix <2,3> &input1, const Matrix <3,2> &input2, 
+                const Matrix <2,2> &expectedResult, const Context &context)
+{
+    // Build program for these specific devices
+    program.build(devices);
+
+    // Make kernel
+    Kernel kernel(program, "vector_add");
+
+    // Create memory buffers
+    Buffer bufferA = Buffer(context, CL_MEM_READ_ONLY, input1.size() * input1[0].size() * sizeof(int));
+    Buffer bufferB = Buffer(context, CL_MEM_READ_ONLY, input2.size() * input2[0].size() * sizeof(int));
+    Buffer bufferC = Buffer(context, CL_MEM_WRITE_ONLY, input1.size() * input2[0].size() * sizeof(int));
+    Buffer bufferK = Buffer(context, CL_MEM_READ_ONLY, sizeof(int));
+
+    // Copy lists A and B to the memory buffers
+    queue.enqueueWriteBuffer(bufferA, CL_TRUE, 0, input1.size() * input1[0].size() * sizeof(int), &input1[0]);
+    queue.enqueueWriteBuffer(bufferB, CL_TRUE, 0, input2.size() * input2[0].size() * sizeof(int), &input2[0]);
+
+    int k = input2.size();
+    queue.enqueueWriteBuffer(bufferK, CL_TRUE, 0, sizeof(int), &k);
+
+    // Set arguments to kernel//
+    kernel.setArg(0, bufferA);
+    kernel.setArg(1, bufferB);
+    kernel.setArg(2, bufferC);
+    kernel.setArg(3, bufferK);
+
+    queue.enqueueNDRangeKernel(kernel, NullRange, NDRange(input1.size(), input2[0].size()), NDRange(1, 1));
+
+    Matrix<2, 2> result = { { {}, {}} };
+    queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, 4 * sizeof(int), &result[0]);
+
+    if (result == expectedResult)
+        std::cout << "SUCCESS";
+    else
+        std::cout << "FAIL";
+}
+
+int main() 
+{
     // Create the two input vectors
-    const int LIST_SIZE = 1000;
-    array<int, LIST_SIZE> A = {};
-    array<int, LIST_SIZE> B = {};
-    for (int i = 0; i < LIST_SIZE; i++) {
-        A[i] = i;
-        B[i] = LIST_SIZE - i;
-    }
+    Matrix<2, 3> input1 = { {
+    {1,2,3},
+    {4,5,6}
+    } };
 
-    try {
-        // Get available platforms
-        vector<Platform> platforms;
-        Platform::get(&platforms);
+    Matrix<3, 2> input2 = { {
+    {7,8},
+    {9,10},
+    {11,12}
+    } };
 
-        // Select the default platform and create a context using this platform and the GPU
-        cl_context_properties cps[3] = {
-            CL_CONTEXT_PLATFORM,
-            (cl_context_properties)(platforms[0])(),
-            0
-        };
-        Context context(CL_DEVICE_TYPE_GPU, cps);
+    Matrix<2, 2> expectedResult = { {
+    {58, 64},
+    {139, 154}
+    } };
 
-        // Get a list of devices on this platform
-        vector<Device> devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    Platform::get(&platforms);
+    cps[0] = CL_CONTEXT_PLATFORM;
+    cps[1] = (cl_context_properties)(platforms[0])();
+    cps[2] = 0;
+    Context context(CL_DEVICE_TYPE_GPU, cps);
+    devices = context.getInfo<CL_CONTEXT_DEVICES>();
+    queue = CommandQueue(context, devices[0]);
+    program = Program(context, source);
 
-        // Create a command queue and use the first device
-        CommandQueue queue = CommandQueue(context, devices[0]);
-
-        // Read source file
-        std::ifstream sourceFile("vector_adds_kernel.cl");
-        std::string sourceCode(
-            std::istreambuf_iterator<char>(sourceFile),
-            (std::istreambuf_iterator<char>()));
-        Program::Sources source(1, std::make_pair(sourceCode.c_str(), sourceCode.length() + 1));
-
-        // Make program of the source code in the context
-        Program program = Program(context, source);
-
-        // Build program for these specific devices
-        program.build(devices);
-
-        // Make kernel
-        Kernel kernel(program, "vector_add");
-
-        // Create memory buffers
-        Buffer bufferA = Buffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int));
-        Buffer bufferB = Buffer(context, CL_MEM_READ_ONLY, LIST_SIZE * sizeof(int));
-        Buffer bufferC = Buffer(context, CL_MEM_WRITE_ONLY, LIST_SIZE * sizeof(int));
-
-        // Copy lists A and B to the memory buffers
-        queue.enqueueWriteBuffer(bufferA, CL_TRUE, 0, LIST_SIZE * sizeof(int), &A[0]);
-        queue.enqueueWriteBuffer(bufferB, CL_TRUE, 0, LIST_SIZE * sizeof(int), &B[0]);
-
-        // Set arguments to kernel
-        kernel.setArg(0, bufferA);
-        kernel.setArg(1, bufferB);
-        kernel.setArg(2, bufferC);
-
-        // Run the kernel on specific ND range
-        NDRange global(LIST_SIZE);
-        NDRange local(1);
-        queue.enqueueNDRangeKernel(kernel, NullRange, global, local);
-
-        // Read buffer C into a local list
-        array<int, LIST_SIZE> C = {};
-        queue.enqueueReadBuffer(bufferC, CL_TRUE, 0, LIST_SIZE * sizeof(int), &C[0]);
-
-        for (int i = 0; i < LIST_SIZE; i++)
-            std::cout << A[i] << " + " << B[i] << " = " << C[i] << std::endl;
-    }
-    catch (Error error) {
-        std::cout << error.what() << "(" << error.err() << ")" << std::endl;
-    }
+    testLab5_2(input1, input2, expectedResult, context);
 
     return 0;
 }
--- matrix_openCL/Project1/vector_adds_kernel.cl
+++ matrix_openCL/Project1/vector_adds_kernel.cl
@@ -1,8 +1,13 @@
-__kernel void vector_add(__global const int *A, __global const int *B, __global int *C) {
+__kernel void vector_add(__global const int *A, __global const int *B, __global int *C, __global const int *k) 
+{
  
     // Get the index of the current element to be processed
     int i = get_global_id(0);
- 
-    // Do the operation
-    C[i] = A[i] + B[i];
+    int j = get_global_id(1);
+
+    int sizeI = get_global_size(0);
+    int sizeJ = get_global_size(1);
+
+    for(int it = 0 ; it < *k ; it++)
+        C[i * sizeJ + j] += A[i * k[0] + it] * B[sizeJ * it + j];
 }
